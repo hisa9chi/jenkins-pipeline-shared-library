b857bf06cdf9f82de56b706c9f09d791872953b1
hisa9chi <eternal.philosophy@gmail.com>
2019-02-08 03:16:42 +0900

    chenge get

--- vars/postHttp.groovy
+++ vars/postHttp.groovy
@@ -1,22 +1,23 @@
+import groovy.json.JsonBuilder
+@Grab("org.jodd:jodd-http:3.8.5")
+import jodd.http.HttpRequest
+
 import java.io.BufferedReader 
 import java.io.InputStreamReader 
 import java.io.OutputStreamWriter 
 import java.net.URL 
 import java.net.URLConnection 
 
-def call(urlString, paramString) { 
-    def url = new URL(urlString) 
-    def conn = url.openConnection() 
-    conn.setDoOutput(true) 
-    def writer = new OutputStreamWriter(conn.getOutputStream()) 
+def call(urlString) { 
+    private HttpRequest httpRequest
+    private String userAgent = 'Jenkins'
 
-    writer.write(paramString) 
-    writer.flush() 
-    String line 
-    def reader = new BufferedReader(new  InputStreamReader(conn.getInputStream())) 
-    while ((line = reader.readLine()) != null) { 
-     println line 
-    } 
-    writer.close() 
-    reader.close() 
-} 
+    JenkinsHttpClient() {
+        httpRequest = new HttpRequest()
+    }
+
+    def resp = httpRequest.get(urlString)
+            .header("User-Agent", userAgent)
+            .send()
+    return resp.bodyText()
+}
