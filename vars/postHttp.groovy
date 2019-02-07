9bafc6e0f7c1019020b1a6d936d31db26b85200b
hisa9chi <eternal.philosophy@gmail.com>
2019-02-08 03:22:37 +0900

    delete unuse import

--- vars/postHttp.groovy
+++ vars/postHttp.groovy
@@ -2,12 +2,6 @@ import groovy.json.JsonBuilder
 @Grab("org.jodd:jodd-http:3.8.5")
 import jodd.http.HttpRequest
 
-import java.io.BufferedReader 
-import java.io.InputStreamReader 
-import java.io.OutputStreamWriter 
-import java.net.URL 
-import java.net.URLConnection 
-
 def call(urlString) { 
     private HttpRequest httpRequest = new HttpRequest()
     private String userAgent = 'Jenkins'
