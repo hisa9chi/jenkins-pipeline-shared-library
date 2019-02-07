ca79e54c83a9bf51ee023ac66b0d6cb055106680
hisa9chi <eternal.philosophy@gmail.com>
2019-02-08 03:46:41 +0900

    リファクタ

--- vars/postHttp.groovy
+++ vars/postHttp.groovy
@@ -1,14 +1,15 @@
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ResponseBody;
+import com.squareup.okhttp;
+//import com.squareup.okhttp.OkHttpClient;
+//import com.squareup.okhttp.Request;
+//import com.squareup.okhttp.Response;
+//import com.squareup.okhttp.ResponseBody;
 
 def call(urlString) { 
     private OkHttpClient client = new OkHttpClient();
 
     Request request = new Request.Builder().url(urlString).build();
     Call call = client.newCall(request);
-    Response response = call.execute();
+    Response response = client.newCall(request).execute();
     ResponseBody body = response.body();
 
     return response
